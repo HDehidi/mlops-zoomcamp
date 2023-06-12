289f6347cc66b2efb1c1f8dcef3375b2a32ddbf3
HDehidi <HDehidi@github.com>
2023-06-12 14:06:53 +0400

    changed flow name

--- 03-orchestration/3.4/orchestrate_hw_q4.py
+++ 03-orchestration/3.4/orchestrate_hw_q4.py
@@ -130,7 +130,7 @@ def train_best_model(
 
 
 @flow
-def main_flow(
+def hw_flow(
     train_path: str = "03-orchestration/data/green_tripdata_2023-02.parquet",
     val_path: str = "03-orchestration/data/green_tripdata_2023-03.parquet",
 ) -> None:
